6845005a66e7b15616cb668dbebcdd5eb045a49a
Ian Wood <72479359+Dev-Wood@users.noreply.github.com>
2022-12-12 18:44:58 +0000

    A whole lotta changes

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+const user = ["user1", "user2", "user3", "user4"];
+const password = ["password1", "password2", "password3", "password4"];
+const grade = ["gr9", "gr10", "gr11", "gr12"];
+const gender = ["male", "female", "other", "male"];
+const club = ["hackclub", "deca", "film", "robotics"];
+
+let userLength = user.length;
+let passwordLength = password.length;
+
 function loginFunction() {
 	//Gets data from input box
-	let username = document.getElementById("username").value;
-	let password = document.getElementById("password").value;
+	let enteredUser = document.getElementById("username").value;
+	let enteredPassword = document.getElementById("password").value;
 
 	//Sends alert if Username or Password fields are blank
-	if (username == "") {
-		alert("Error: Username or Password field was blank")
-		return
-	}
-	else if (password == "") {
+	if (enteredUser == "" || enteredPassword == "") {
 		alert("Error: Username or Password field was blank")
 		return
 	}
 	else
-
-		console.log(username);
-		console.log(password);
-
+		//Compares entered username to one from array
+		for (let i = 0; i < userLength; i++) {
+    	if (enteredUser == user[i] && enteredPassword == password[i]) {
+				console.log(user[i]);
+				console.log(password[i]);
+				location.href = '/teacher.html';
+				return
+			} 
+		}
+		alert("Error: Username or Password was incorrect")
 }
